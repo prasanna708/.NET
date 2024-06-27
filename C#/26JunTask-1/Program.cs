e6ce72bd2349ef9cec2a4c852c3250dd246bd185
prasannakumar4301 <prasannakumar4301@gmail.com>
2024-06-27 16:54:12 +0530

    v1

---
+++ C#/26JunTask-1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.IO;
+
+namespace _26JunTask_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a program to accept roll no , name , marks
+            // in 3 subjects and write into file called Student.txt
+            try
+            {
+                FileStream fs = new FileStream(@"D:\files\Student.txt", FileMode.Create,FileAccess.Write);
+                StreamWriter sw = new StreamWriter(fs);
+               
+                Console.WriteLine("Enter roll no. of Student: ");
+                int Rno = Convert.ToInt32(Console.ReadLine());
+                sw.Write(Rno + "\t");
+
+                Console.WriteLine("Enter Name of Student: ");
+                string name = Console.ReadLine();
+                sw.Write(name + "\t");
+
+                Console.WriteLine("Enter marks of student in 3 subjects: ");
+                int M = Convert.ToInt32(Console.ReadLine());
+                int P = Convert.ToInt32(Console.ReadLine());
+                int C = Convert.ToInt32(Console.ReadLine());
+                sw.Write(M + " ");
+                sw.Write(P + " ");
+                sw.Write(C + " ");
+
+                sw.Flush();
+                sw.Close();
+                fs.Close();
+                Console.WriteLine("File Created Successfully...........");
+                
+
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/26JunTask-2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _26JunTask_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Write a program to read the Student.txt and
+            //display total and average from the marks.
+
+            try
+            {
+                FileStream fs = new FileStream(@"D:\files\Student.txt",FileMode.Open,FileAccess.Read);
+                StreamReader sr = new StreamReader(fs);
+                //Reading into File
+                String RNo = sr.ReadLine();
+                String name = sr.ReadLine();    
+                string M = sr.ReadLine();
+                string P = sr.ReadLine();
+                string C = sr.ReadLine();
+                
+                Console.WriteLine("Reading data from file.....\n ");
+                Console.WriteLine(RNo);
+                Console.WriteLine(name);
+                Console.WriteLine(M);
+                Console.WriteLine(P);
+                Console.WriteLine(C);
+               
+                sr.Close();
+                fs.Close();
+
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex1) 
+            {
+                Console.WriteLine(ex1.Message);  
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
