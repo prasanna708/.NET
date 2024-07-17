bc752081c6f8afe6b3c0601f5d74357b4e1d53ab
prasannakumar4301 <prasannakumar4301@gmail.com>
2024-07-17 15:59:20 +0530

    v1

---
+++ C#/Numbers/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers
+{
+    public class MyClass
+    {
+            private int _num;
+            public MyClass()
+            {
+                _num = 0;
+            }
+            public MyClass(int num)
+            {
+                _num = num;
+            }
+            public void Accept(int num)
+            {
+                _num = num;
+            }
+            //Even or Odd number
+            public bool IsEven()
+            {
+                if (_num % 2 == 0)
+                    return true;
+                else
+                    return false;
+            }
+            //Palindrome number
+            public bool IsPalindrome()
+            {
+                int rev = 0;
+                int temp = _num;
+                while (temp > 0)
+                {
+                    int rem = temp % 10;
+                    rev = rev * 10 + rem;
+                    temp = temp / 10;
+                }
+                return rev == _num;
+            }
+            //Armstrong number
+            public bool IsArmstrong()
+            {
+                double sum = 0;
+                int temp = _num;
+                int count = 0;
+                while (temp > 0)
+                {
+                    count++;
+                    temp = temp / 10;
+                }
+                temp = _num;
+                while (temp > 0)
+                {
+                    int r = temp % 10;
+                    sum += Math.Pow(r, count);
+                    temp = temp / 10;
+                }
+                return sum == _num;
+            }
+
+        
+    }
+}
+
---
+++ C#/Test/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Numbers;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass(333);
+            //Even or Odd Number
+            if (myClass.IsEven())
+                Console.WriteLine("Even");
+            else
+                Console.WriteLine("Odd");
+            //Palindrome number
+            if (myClass.IsPalindrome())
+                Console.WriteLine("Palindrome");
+            else
+                Console.WriteLine("Not Palindrome");
+            //Armsrtong Number
+            if (myClass.IsArmstrong())
+                Console.WriteLine("Armstrong");
+            else
+                Console.WriteLine("Not Armstrong");
+
+            Console.ReadKey();
+        }
+    }
+}
